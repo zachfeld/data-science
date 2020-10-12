61d6a9d4b8ef4812194de89e2bd856be81f262ea
zfeldman7 <feldman_zach@bentley.edu>
2020-10-12 00:02:13 -0400

    finished up to question 4

--- Project 1/Project1.py
+++ Project 1/Project1.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import random
+import numpy as np
 ## import data
 election_2016 = pd.read_csv('election_2016.csv')
 covid_county = pd.read_csv('covid_confirmed_usafacts.csv')
@@ -25,12 +26,55 @@ df = df.loc[:, ~df.columns.isin(exclude_counties)]
 # ~~~~~~   Question 4 - Applying a SciPy's curve fit function   ~~~~~~
 ## Apply SciPy curve_fit function to each row, returning three beta values for each row,
 ## and create three columns for those three beta values
-import math
-def my_model ( x, β0, β1, β2 ):
+def logistic_curve ( x, β0, β1, β2 ):
     # β0 - max # of cases in long term
     # β1 - measure of the rate of spread
     # β2 - time of maximum spread
-    return β0 / (1 + math.exp(β1 * (-x + β2) ) )
+    return β0 / (1 + np.exp(β1 * (-x + β2) ) )
+
+def bad_guess (data):
+    # β0 - max number of cases seen so far
+    # β1 - 1
+    # β2 - date range / 2
+    date_rows = data.iloc[:,3:]
+    
+    # make guesses based on the criteria listed above
+    data['β0'] = date_rows.max(axis=1)
+    data['β1'] = [1] * len(data)
+    data['β2'] = (len(date_rows.columns) / 2)
+
+    return data
+
+from scipy.optimize import curve_fit
+import  matplotlib.pyplot as plt
+
+# but all of our guessed betas into the dataframe
+data = bad_guess(df)
+
+# loop through every state in the data frame
+indicies = data.index
+for index in indicies:
+
+    state = data.loc[index]
+
+    # get the guessed betas, xs and ys
+    my_guessed_betas = [state.loc['β0'], state.loc['β1'], state.loc['β2']]
+    xs = np.arange(0,len(state)-6)
+    ys = state.iloc[3:len(state)-3]
+
+    # fit the logistic curve to the data, if we can't throw an exception
+    try:
+
+        found_betas, covariance = curve_fit( logistic_curve, xs, ys.astype('int'), p0=my_guessed_betas )
+        β0, β1, β2 = found_betas
+        print(covariance)
+    except Exception as e:
+        print(f'state: {index}: ', e)
+
+# for problem 5, we need to make a dataframe with pop, clinton, trump, all 3 of the calculated b's, 
+# most recent number of cases, most recent number of cases per cap, political leaning ratio
+
+
 
 ## Export to Excel for viewing
 #df.to_excel(r'Project 1\state_election_covid.xlsx')
